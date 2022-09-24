94ebb75d25699833ae50e1fcc5d75d9e970941f7
Mekala Chandu <114248527+ChanduMekala-187235@users.noreply.github.com>
2022-09-24 19:06:00 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,21 @@
+package helloworld.java;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0;
+		
+		
+		for(int i=1;i<=50;++i)
+		{
+			
+			
+			sum=sum+i;
+			
+			
+		}
+		System.out.println("Sum of first 50 natural numbers is="+sum);
+	}
+
+}
